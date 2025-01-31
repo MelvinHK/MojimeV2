87827576ea4c4fb571f027a2762d99d37739010e
Melvin <melvinhkusuma@gmail.com>
2025-01-31 14:55:20 +1100

    added hls to video player

--- MojimeV2/src/components/VideoPlayer.tsx
+++ MojimeV2/src/components/VideoPlayer.tsx
@@ -1,16 +1,30 @@
-import { useEffect } from "react"
+import { useRef, useEffect } from "react";
+import Hls from 'hls.js';
 
 interface VideoPlayerProps {
   source: string
 }
 
 function VideoPlayer(props: VideoPlayerProps) {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const hlsRef = useRef<Hls | null>(null); // Holds the Hls instance
+
   useEffect(() => {
-    console.log(props.source);
-  }, [])
+    if (videoRef.current && Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(props.source);
+      hls.attachMedia(videoRef.current);
+      hlsRef.current = hls;
+
+      return () => {
+        hlsRef.current && hlsRef.current.destroy();
+      };
+    }
+  }, [props.source]);
+
 
   return (
-    <video></video>
+    <video ref={videoRef} controls></video>
   )
 }
 
