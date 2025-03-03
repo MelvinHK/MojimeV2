890d71e30c81bffae3ebde8439d00fee2f644583
Melvin <melvinhkusuma@gmail.com>
2025-03-03 15:41:22 +1100

    added scrubbing preview

--- MojimeV2/src/components/VideoPlayer/ProgressBar.tsx
+++ MojimeV2/src/components/VideoPlayer/ProgressBar.tsx
@@ -8,6 +8,9 @@ function ProgressBar() {
         <TimeSlider.TrackFill className="media-slider-track-fill media-slider-track" />
         <TimeSlider.Progress className="media-slider-progress media-slider-track" />
       </TimeSlider.Track>
+      <TimeSlider.Preview className="media-slider-preview">
+        <TimeSlider.Value className="media-slider-value" />
+      </TimeSlider.Preview>
       <TimeSlider.Thumb className="media-slider-thumb" />
     </TimeSlider.Root>
   )
