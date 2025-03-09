4b607789b24737a158bb912f171919a0137b1e2d
Melvin <melvinhkusuma@gmail.com>
2025-03-09 15:27:52 +1100

    port configurability

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import router from './routes';
 
 const app = express();
+const PORT = 5000;
 
 app.use((_req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -10,6 +11,6 @@ app.use((_req, res, next) => {
 
 app.use('/api', router);
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
