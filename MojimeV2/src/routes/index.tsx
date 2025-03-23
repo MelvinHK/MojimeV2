3eb1cf0ca8d101945f7843b5e79217f10c295c17
Melvin <melvinhkusuma@gmail.com>
2025-03-24 00:51:22 +1100

    added kaomojis

--- MojimeV2/src/routes/index.tsx
+++ MojimeV2/src/routes/index.tsx
@@ -1,13 +1,26 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useState, useEffect } from 'react';
 
 export const Route = createFileRoute('/')({
   component: Index,
 })
 
+const kaomojis = [
+  "(≧ ▽ ≦)", "(=・ω・=)", "(● ˇ∀ˇ ●)", "(*✧ω✧*)"
+];
+
 function Index() {
+  const [kaomoji, setKaomoji] = useState("");
+
+  useEffect(() => {
+    const randomKaomoji = kaomojis[Math.floor(Math.random() * kaomojis.length)];
+    setKaomoji(randomKaomoji);
+  }, []);
+
   return (
     <div className="home-container">
-      Mojime
+      <div>{kaomoji}</div>
+      <div>Mojime</div>
     </div>
-  )
+  );
 }
