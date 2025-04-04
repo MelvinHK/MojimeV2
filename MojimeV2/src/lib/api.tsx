1bbe53469878937a14de0787bb6536bfe7b56bb2
Melvin <melvinhkusuma@gmail.com>
2025-04-04 16:33:15 +1100

    update api urls

--- MojimeV2/src/lib/api.tsx
+++ MojimeV2/src/lib/api.tsx
@@ -4,20 +4,20 @@ import { SearchResult, Anime, Source } from "../models";
 const baseURL = 'https://mojime-server.vercel.app/api';
 
 export async function getSearch(query: string): Promise<SearchResult[]> {
-  const results = await axios.get(`${baseURL}/search?query=${query}`);
+  const results = await axios.get(`${baseURL}/kai/search?query=${query}`);
   return results.data;
 }
 
 export async function getAnime(id: string): Promise<Anime> {
-  const episodes = await axios.get(`${baseURL}/anime?animeId=${id}`);
+  const episodes = await axios.get(`${baseURL}/kai/anime?animeId=${id}`);
   return episodes.data;
 }
 
 export async function getEpisode(id: string): Promise<Source> {
-  const source = await axios.get(`${baseURL}/episode?episodeId=${id}`);
+  const source = await axios.get(`${baseURL}/kai/episode?episodeId=${id}`);
   return source.data;
 }
 
 export function getProxyURL(url: string) {
-  return `${baseURL}/proxy?url=${url}&rewrite=qualities`;
+  return `${baseURL}/kai/proxy?url=${url}&rewrite=qualities`;
 }
